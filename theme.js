dbde69272c0afc32281874bdd3641137c23f71f0
Enejanshyhyyeva <134204691+Enejanshyhyyeva@users.noreply.github.com>
2023-07-03 17:54:32 +0500

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+export const theme=createTheme({
+    palette:{
+        mode:'light',
+        primary:{                                 //1nji derejeli
+            main:'#003468' ,  ///esasy rengk
+            dark:'#000000',
+           
+
+            light:'#FFFFFF',
+            contrastText:'#0000004A'
+
+        }
+        // secondary:{}
+    }
+
+})
