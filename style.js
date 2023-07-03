89021be0bad5f3b9a9039b5a0c0b21e8dc7a59fd
Enejanshyhyyeva <134204691+Enejanshyhyyeva@users.noreply.github.com>
2023-07-03 17:55:18 +0500

    Create style.js

---
+++ style.js
@@ -0,0 +1,16 @@
+import { theme } from "./theme";
+
+export const componentTitle={
+fontStyle: "normal",
+fontWeight: "400",
+fontSize: "32px",
+lineHeight: "39px",
+color:theme.palette.primary.main,
+border:"2.50px solid black"
+
+
+
+
+
+
+};
